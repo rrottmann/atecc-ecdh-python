3dc42786ceb04e467832e03f10e5a56f73b13dce
Reiner Rottmann <reiner@rottmann.it>
2021-03-07 10:06:35 +0100

    Added atecc608a device init.

--- genkey_device.py
+++ genkey_device.py
@@ -1,5 +1,15 @@
 from cryptoauthlib import *
 
+ATCA_SUCCESS = 0x00
+load_cryptoauthlib()
+
+cfg = eval('cfg_ateccx08a_i2c_default()')
+
+cfg.cfg.atcai2c.bus = 1
+cfg.cfg.atcai2c.slave_address = 0x6A
+
+assert atcab_init(cfg) == ATCA_SUCCESS
+
 ec256_device_pub = bytearray(64)
 
 # Using tempkey slot 0xFFFF/65535 and deriving ecdh_secret
